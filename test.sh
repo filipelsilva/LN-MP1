6711baa06f22b65abd3c183083e163b64aa6d759
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-09-19 11:52:27 +0100

    removed extra test method

--- test.sh
+++ test.sh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 
-# # Method 1
-# for w in compiled/t-$1-*.fst; do \
-#     fstcompose $w compiled/$1.fst | fstshortestpath | fstproject --project_type=output | fstrmepsilon | fsttopsort > compiled/$(basename $w ".fst")-out.fst ; \
-# done
-# for i in compiled/t-$1-*-out.fst; do \
-#     echo "Creating image: images/$(basename $i '.fst').pdf" ; \
-#     fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf ; \
-# done
-
-# Method 3
 fst2word() {
     awk '{if(NF>=3){printf("%s",$3)}}END{printf("\n")}'
 }
